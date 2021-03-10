f28cbd5f2d20c8bbb9987996635c2f5280a6b872
ZyCromerZ <neetroid97@gmail.com>
2021-03-10 20:22:44 +0700

    do-now: building GCC 11
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- do-now.sh
+++ do-now.sh
@@ -8,10 +8,10 @@ rm -rf .git
 git clone "https://${GIT_SECRETB}@github.com/ZyCromerZ/gdrive_uploader" gdrive_uploader
 chmod +x ./gdrive_uploader/run.sh
 
-./build -a arm64 -s gnu -v 10 -p gz
-./gdrive_uploader/run.sh "$(pwd)/aarch64-linux-gnu-10.x-gnu-$(date +%Y%m%d).tar.gz" "keqing-drive" "ALL-COMPILED-GCC"
-
-# ./build -a arm -s gnu -v 10 -p gz
-# ./gdrive_uploader/run.sh "$(pwd)/${PACKAGE}" "keqing-drive" "ALL-COMPILED-GCC"
+./build -a arm64 -s gnu -v 11 -p gz
+FILE="$(pwd)/aarch64-linux-gnu-11.x-gnu-$(date +%Y%m%d).tar.gz"
+cd gdrive_uploader
+. run.sh "$FILE" "keqing-drive" "ALL-COMPILED-GCC"
+cd ..
 
 rm -rf *
