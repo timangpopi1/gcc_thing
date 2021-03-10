e070e68dae9fe751fe913cd9b40aefee1677ea72
ZyCromerZ <neetroid97@gmail.com>
2021-03-10 20:15:54 +0700

    do-now: compiled aarch64 only
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- do-now.sh
+++ do-now.sh
@@ -5,55 +5,13 @@ apt-get -y install flex bison ncurses-dev texinfo gcc gperf patch libtool automa
 
 # CurrentMainPath="$(pwd)"
 rm -rf .git
+git clone "https://${GIT_SECRETB}@github.com/ZyCromerZ/gdrive_uploader" gdrive_uploader
+chmod +x ./gdrive_uploader/run.sh
 
-PushChanges(){
-    dumpGCCVer="$(./bin/aarch64-linux-gnu-gcc -dumpversion)"
-    GitBranch="${dumpGCCVer}-$(date +"%Y%m%d")"
-    GCCVer="$(./bin/aarch64-linux-gnu-gcc --version | head -n 1)"
-    git checkout -b "${GitBranch}"
-    git config http.version HTTP/1.1
-    echo "# quick INFO aarch64-linux-gnu"> readme.md
-    echo "* Compiled with :" >> readme.md
-    echo "  * BINUTILS version: ${BINUTILS_tar}" >> readme.md
-    echo "  * GMP version: ${GMP}" >> readme.md
-    echo "  * MPFR version: ${MPFR}" >> readme.md
-    echo "  * MPC version: ${MPC}" >> readme.md
-    echo "  * ISL version: ${ISL}" >> readme.md
-    echo "  * GCLIB version: ${GLIBC}" >> readme.md
-    git add . && git commit -s -m "aarch64 GCC '$GCCVer'" && git push -f origin "${GitBranch}"
-    git config http.version HTTP/2
-}
+./build -a arm64 -s gnu -v 10 -p gz
+./gdrive_uploader/run.sh "$(pwd)/${PACKAGE}" "keqing-drive" "ALL-COMPILED-GCC"
 
-PushChangesArm(){
-    dumpGCCVer="$(./bin/arm-linux-gnueabi-gcc -dumpversion)"
-    GitBranch="${dumpGCCVer}-$(date +"%Y%m%d")"
-    GCCVer="$(./bin/arm-linux-gnueabi-gcc --version | head -n 1)"
-    git checkout -b "${GitBranch}"
-    git config http.version HTTP/1.1
-    echo "# quick INFO arm-linux-gnueabi"> readme.md
-    echo "* Compiled with :" >> readme.md
-    echo "  * BINUTILS version: ${BINUTILS_tar}" >> readme.md
-    echo "  * GMP version: ${GMP}" >> readme.md
-    echo "  * MPFR version: ${MPFR}" >> readme.md
-    echo "  * MPC version: ${MPC}" >> readme.md
-    echo "  * ISL version: ${ISL}" >> readme.md
-    echo "  * GCLIB version: ${GLIBC}" >> readme.md
-    git add . && git commit -s -m "arm GCC '$GCCVer'" && git push -f origin "${GitBranch}"
-    git config http.version HTTP/2
-}
-
-./build -a arm64 -s gnu -v 10
-
-cd aarch64-linux-gnu
-git init && git remote add origin https://${GIT_SECRETB}@github.com/ZyCromerZ/aarch64-linux-gnu.git
-PushChanges
-cd ..
-
-./build -a arm -s gnu -v 10
-
-cd arm-linux-gnueabi
-git init && git remote add origin https://${GIT_SECRETB}@github.com/ZyCromerZ/arm-linux-gnueabi.git
-PushChangesArm
-cd ..
+# ./build -a arm -s gnu -v 10 -p gz
+# ./gdrive_uploader/run.sh "$(pwd)/${PACKAGE}" "keqing-drive" "ALL-COMPILED-GCC"
 
 rm -rf *
